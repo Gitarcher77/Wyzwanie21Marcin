4b656060a5a5932ef58623bda59fa177a8c89669
Marcin Wojciechowski <marcinwojciechowski721@gmail.com>
2023-08-27 14:38:11 +0200

    zadanie domowe dzień 4

---
+++ Program.cs/Program.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿string name = "Marcin";
+string sex = "Mężczyzna";
+int age = 15;
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && sex != "Kobieta")
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
